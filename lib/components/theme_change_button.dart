1c1b73fcd91df6c1ce9d601e855cf2629dc5f365
LokmanHossen <lokmanhossen230@gmail.com>
2024-05-23 23:03:53 +0600

    ThemeChangeyButton

---
+++ lib/components/theme_change_button.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/theme_controller.dart';
+
+class ThemeChangeButton extends StatelessWidget {
+  const ThemeChangeButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeController themeController = Get.put(ThemeController());
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          // height: 50,
+          padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            borderRadius: BorderRadius.circular(24),
+          ),
+          child: InkWell(
+            onTap: () {
+              themeController.changeTheme();
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    themeController.changeTheme();
+                  },
+                  icon: Icon(
+                    Icons.light_mode,
+                    color: themeController.isDark.value
+                        ? Theme.of(context).colorScheme.onSecondaryContainer
+                        : Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    themeController.changeTheme();
+                  },
+                  icon: Icon(
+                    Icons.dark_mode,
+                    color: themeController.isDark.value
+                        ? Theme.of(context).colorScheme.primary
+                        : Theme.of(context).colorScheme.onSecondaryContainer,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
