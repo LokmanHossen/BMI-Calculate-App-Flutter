59125c6a3491fae95507d87e2016e0f5deabc511
LokmanHossen <lokmanhossen230@gmail.com>
2024-05-24 01:29:14 +0600

    Update

--- lib/components/primary_button.dart
+++ lib/components/primary_button.dart
@@ -2,7 +2,7 @@ import 'package:bmi_app/controllers/bmi_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class PrimaryButton extends StatelessWidget {
+class PrimaryButton extends StatefulWidget {
   final IconData icon;
   final String buttonText;
   final VoidCallback onPressed;
@@ -13,35 +13,49 @@ class PrimaryButton extends StatelessWidget {
       required this.onPressed});
 
   @override
+  State<PrimaryButton> createState() => _PrimaryButtonState();
+}
+
+class _PrimaryButtonState extends State<PrimaryButton> {
+  @override
   Widget build(BuildContext context) {
     BmiController bmiController = Get.put(BmiController());
     return Expanded(
       child: InkWell(
-        onTap: onPressed,
-        child: Container(
-          height: 50,
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(
+        onTap: widget.onPressed,
+        child: Obx(
+          () => Container(
+            height: 50,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15),
-              color: Theme.of(context).colorScheme.primary),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                icon,
-                color: Theme.of(context).colorScheme.surface,
-              ),
-              const SizedBox(width: 8),
-              Text(
-                buttonText,
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.8,
-                  color: Theme.of(context).colorScheme.surface,
+              color: bmiController.genders.value == widget.buttonText
+                  ? Theme.of(context).colorScheme.primary
+                  : Theme.of(context).colorScheme.primaryContainer,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  widget.icon,
+                  color: bmiController.genders.value == widget.buttonText
+                      ? Theme.of(context).colorScheme.primaryContainer
+                      : Theme.of(context).colorScheme.primary,
                 ),
-              )
-            ],
+                const SizedBox(width: 8),
+                Text(
+                  widget.buttonText,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.8,
+                    color: bmiController.genders.value == widget.buttonText
+                        ? Theme.of(context).colorScheme.primaryContainer
+                        : Theme.of(context).colorScheme.primary,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
