24b7d394354976d6c2f62ec2307452808fa43b4b
LokmanHossen <lokmanhossen230@gmail.com>
2024-05-23 22:59:50 +0600

    HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,98 @@
+import 'package:bmi_app/components/age_selector.dart';
+import 'package:bmi_app/components/height_selector.dart';
+import 'package:bmi_app/components/primary_button.dart';
+import 'package:bmi_app/components/theme_change_button.dart';
+import 'package:bmi_app/components/weight_selector.dart';
+import 'package:bmi_app/controllers/theme_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ThemeController themeController = Get.put(ThemeController());
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       body: SafeArea(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Change Theme"),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              const ThemeChangeButton(),
+              Row(
+                children: [
+                  Text(
+                    "Welcome😊",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Theme.of(context).colorScheme.onSecondaryContainer,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    "BMI Calculator",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  PrimaryButton(
+                    onPressed: () {},
+                    icon: Icons.male,
+                    buttonText: "Male",
+                  ),
+                  const SizedBox(width: 10),
+                  PrimaryButton(
+                    onPressed: () {},
+                    icon: Icons.female,
+                    buttonText: "Female",
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    HeightSelector(),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          WeightSelector(),
+                          SizedBox(height: 20),
+                          AgeSelector(),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 50,
+                child: PrimaryButton(
+                  icon: Icons.done,
+                  buttonText: "Lets go",
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
